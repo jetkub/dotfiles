c1307ee2f69ba4883542b41b060d75d5c9b532d4
Jakub <mail@jpawel.com>
2026-04-26 00:21:49 -0400

    Function: overwrite pass --clip to fix secret leak
    When I use `pass --clip` to copy a password to my clipboard it is
    supposed to be cleared from your clipboard after 45 seconds. However,
    the password was being written to Maccy, my clipboard manager. Secrets
    in the clipboard manager is a no go. This function disables Maccy when
    `pass --clip|-c` is used and enables it shortly after. This happens
    completely transparent to the user.

--- .zsh/zsh_functions/macos.zsh
+++ .zsh/zsh_functions/macos.zsh
@@ -1,3 +1,31 @@
+# Helper func to get macOS bundle id. This is useful if I need to 
+# configure Maccy to ignore certain application pasteboards which 
+# are identified by their bundle id
 get_bundleid() {
 	osascript -e "id of app \"$1\""
+}
+
+# Overwrite `pass --clip` to fix issue with secrets appearing in
+# my clipboard manager Maccy
+pass() {
+    local clip=0
+    for arg in "$@"; do
+        if [[ "$arg" == "--clip" || "$arg" == "-c" ]]; then
+            clip=1
+            break
+        fi
+    done
+
+    if [[ $clip -eq 1 ]]; then
+        defaults write org.p0deje.Maccy ignoreEvents true
+        command pass "$@"
+        # Fork a background subshell ( ) that waits 2 seconds before
+        # re-enabling Maccy. The last `&` backgrounds the subshell so that 
+        # control is returned to the terminal immediately and doesn't block. 
+        # Here, `&|` backgrounds and *disowns* the job so zsh prints no 
+        # "[N] PID" or "[3]  + done" noise.
+		(sleep 2 && defaults write org.p0deje.Maccy ignoreEvents false) &|
+    else
+        command pass "$@"
+    fi
 }
